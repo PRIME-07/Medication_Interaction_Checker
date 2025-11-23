10ec9f82adf8bdac96c3dc67b9afe73816e2c158
PRIME-07 <anujm0707@gmail.com>
2025-11-24 00:10:47 +0530

    added code to support nrgok and github pages

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://95a17939b18b.ngrok-free.app';
 
 export const searchDrugs = async (query) => {
   try {
